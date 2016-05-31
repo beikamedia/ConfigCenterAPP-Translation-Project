73e01a98ae9b64ab5f655891e7eb3f6cec5573a6
dennis <dennis@akashia.de>
2016-05-31 15:58:22 +0200

    add login lang file (de)

---
+++ login/va-de.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+*	ConfigCenter OpenLang Script 
+*	(C) VisualArts/BeikaMedia
+*/
+
+// ---- header ------------------------
+
+	$header_lang['brand_header'] = "VisualArts/BeikaMedia";
+	$header_lang['title_header'] = "ConfigCenterAPP";
+
+// ---- content -----------------------
+
+	$content_lang['login_header'] = "Benutzer Login";
+	$content_lang['no_acc'] = "kein Benutzerkonto?";
+	$content_lang['pass_reset'] = "Passwort vergessen?";
+	$content_lang['input_user_place'] = "BenutzerName";
+	$content_lang['input_pass_place'] = "Passwort";
+
+// ---- footer ------------------------
+
+	$footer_lang['data'] = "Datenschutz";
+	$footer_lang['impress'] = "Impressum";
+	$footer_lang['data_desc'] = "Offizielle BeikaMedia Datenschutzerklärung";
+	$footer_lang['impress_desc'] = "Offizielle BeikaMedia Impressum";
+
+?>
